6dc418c9ef8e10894d696f55ec111608305a7af7
ShetSahil <20104122.sahilshetty@gmail.com>
2023-06-05 19:49:42 +0530

    creating login in auth.js

--- Routes/auth.js
+++ Routes/auth.js
@@ -51,4 +51,41 @@ router.post('/createuser',[
     }
 
 })
+
+
+//authenticating the user
+
+router.post('/login',[
+
+    body('email', 'Enter you valid email').isEmail(),
+    body('password', 'Password must be minimum 5 characters long').exists(),
+
+],async(req,res)=>{
+    //if errors,return bad errors
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+    const{email,password}=req.body;
+    try {
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error:"Enter Valid Credentials"})
+        }
+        const passwordCompare = await  bcryptjs.compare(password,user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error:"Enter Valid Credentials"})
+        }
+        const data={
+            user:{
+                id:user.id
+            }
+        }
+        const authtoken=jwt.sign(data,JWT_SECRET);
+        res.json(authtoken)
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Internal errorr");
+        }
+})
 module.exports= router
